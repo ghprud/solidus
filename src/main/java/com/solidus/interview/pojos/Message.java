67bb799aae6136e6e2ecb15f79b7e67ba1ceeb6f
DPSQUARE <ppd@PPDs-MacBook-Pro.local>
2020-03-04 09:27:33 -0500

    fixing the missing constructor

--- src/main/java/com/solidus/interview/pojos/Message.java
+++ src/main/java/com/solidus/interview/pojos/Message.java
@@ -3,6 +3,10 @@ package com.solidus.interview.pojos;
 public class Message {
 	public String message;
 	
+	public Message(){
+
+	}
+	
 	public Message(String message) {
 		this.message = message;
 	}
